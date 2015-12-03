4b524fd72b55e8c20726bde6d0732d3c8f0ed8d4
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-12-03 16:26:54 +0100

    Add solution for the second task of day 3

--- day3/day3.rkt
+++ day3/day3.rkt
@@ -1,22 +1,56 @@
 #lang racket
+(require scheme/mpair)
 
-(define (visit-houses input)
-  (define x 0)
-  (define y 0)
+(define (visit-houses input task)
+  ;; Sleigh driver positions
+  (define human (mcons 0 0))
+  (define robot (mcons 0 0))
+  (define driver 'human)
+
   ;; (0 . 0) is always in the list
-  (define houses (list (cons 0 0))) 
+  (define houses (list (cons 0 0)))
+
+  (define (up)
+    (if (eq? driver 'human)
+        (set-mcdr! human (+ (mcdr human) 1))
+        (set-mcdr! robot (+ (mcdr robot) 1))))
+
+  (define (down)
+    (if (eq? driver 'human)
+        (set-mcdr! human (- (mcdr human) 1))
+        (set-mcdr! robot (- (mcdr robot) 1))))
+
+  (define (left)
+    (if (eq? driver 'human)
+        (set-mcar! human (- (mcar human) 1))
+        (set-mcar! robot (- (mcar robot) 1))))
+
+  (define (right)
+    (if (eq? driver 'human)
+        (set-mcar! human (+ (mcar human) 1))
+        (set-mcar! robot (+ (mcar robot) 1))))
 
   (define (visit-one-house dir)
-    (cond [(char=? dir #\^) (set! y (+ y 1))]
-          [(char=? dir #\v) (set! y (- y 1))]
-          [(char=? dir #\<) (set! x (- x 1))]
-          [(char=? dir #\>) (set! x (+ x 1))]
+    (cond [(char=? dir #\^) (up)]
+          [(char=? dir #\v) (down)]
+          [(char=? dir #\<) (left)]
+          [(char=? dir #\>) (right)]
           [else 'error-bad-input])
 
-    (let ((current-house (cons x y)))
+    (let ((current-house ;; mcons->cons is ugly?
+           (if (eq? driver 'human)
+               (cons (mcar human) (mcdr human))
+               (cons (mcar robot) (mcdr robot)))))
       (if (eq? (member current-house houses) #f)
           (set! houses (append houses (list current-house)))
-          #f)))
+          #f))
+
+    ;; Switch driver if this is assignment 2
+    (if (eq? task 2)
+        (if (eq? driver 'human)
+            (set! driver 'robot)
+            (set! driver 'human))
+        #f))
 
   (sequence-for-each visit-one-house input)
 
@@ -24,13 +58,17 @@
   )
 
 ;; test cases
-;; (visit-houses "^>v<")
-;; (visit-houses ">")
-;; (visit-houses "^v^v^v^v^v")
-;; (visit-houses ">>") ;; 3
-;; (visit-houses "<><><>") ;; 2
+;; (visit-houses "^>v<" 2)
+;; (visit-houses ">" 2)
+;; (visit-houses "^v" 2)
+;; (visit-houses "<><><>" 2) ;; 2
 
+(display "Task 1: ")
 (call-with-input-file "day3input"
   (λ (in)
-    (visit-houses (in-input-port-chars in))))
+    (visit-houses (in-input-port-chars in) 1)))
 
+(display "Task 2: ")
+(call-with-input-file "day3input"
+  (λ (in)
+    (visit-houses (in-input-port-chars in) 2)))
