08b92948d8a99c733fe6782753d5155d07d41039
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-12-11 21:06:53 +0100

    Solve day 9 part 1
    Part 2 is finding the maximum route, which is a change from
    min -> max in line 65

--- day9/day9.rkt
+++ day9/day9.rkt
@@ -1,8 +1,23 @@
 #lang racket
 
 (define all-distances (make-hash))
+(define all-cities (list))
+(define all-routes (list))
 
 (struct destination (city distance))
+(struct route (path distance))
+
+(define (traveling-santa current-city visited total-distance)
+  (let* ([visited+ (append visited (list current-city))]
+         [next-cities (remove* visited+ all-cities)])
+    (if (empty? next-cities)
+        (set! all-routes (append all-routes (list (route visited+ total-distance))))
+        (for ([city-name next-cities])
+          (let* ([city-dest (car (filter
+                                  (λ (c) (string=? city-name (destination-city c)))
+                                  (hash-ref all-distances current-city)))]
+                 [dist (+ total-distance (destination-distance city-dest))])
+            (traveling-santa city-name visited+ dist))))))
 
 (define (already-added? from to)
   (let ([distances (hash-ref all-distances from #f)])
@@ -29,7 +44,7 @@
         (begin (let* ([line-as-vec (list->vector (string-split line))]
                       [from (vector-ref line-as-vec 0)]
                       [dest (destination (vector-ref line-as-vec 2)
-                                         (vector-ref line-as-vec 4))])
+                                         (string->number (vector-ref line-as-vec 4)))])
                  (add-distance from dest))
                (process-input in-port)))))
 
@@ -40,5 +55,20 @@
 (for ([(k v) all-distances])
   (let ([cities (map destination-city v)])
     (let ([distances (map destination-distance v)])
-      (printf "~a:\t ~a\n\t\t ~a\n" k cities distances))))
+      (set! all-cities (append all-cities (list k))))))
+
+
+(for ([city all-cities])
+  (traveling-santa city empty 0))
+
+(length all-routes)
+(define min-route (apply min (map route-distance all-routes)))
+(define (output-route rt)
+  (display (route-path rt))
+  (display ", ")
+  (display (route-distance rt))
+  (newline))
+
+(map output-route (filter (λ (distance)
+                            (eq? min-route (route-distance distance))) all-routes))
 
