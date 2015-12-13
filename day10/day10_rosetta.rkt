a9d3ca0346c1fb9619638636a2f5dc1161fe5676
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-12-13 01:47:25 +0100

    Small changes to day 10
    Mark doesn't have anything to do with this

--- day10/day10_rosetta.rkt
+++ day10/day10_rosetta.rkt
@@ -7,10 +7,10 @@
                    (lambda (m c)
                      (~a (string-length m) c))))
 
-(define (mark str iter max-iter)
+(define (look-and-say str iter max-iter)
   (if (> iter max-iter)
       (length (string->list str))
-      (mark (encode str) (add1 iter) max-iter)))
+      (look-and-say (encode str) (add1 iter) max-iter)))
 
-(time (mark "1113122113" 1 40))
-(time (mark "1113122113" 1 50))
+(time (look-and-say "1113122113" 1 40))
+(time (look-and-say "1113122113" 1 50))
