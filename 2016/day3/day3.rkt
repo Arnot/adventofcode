9e3199b9933b654586caab089deb2e2dd0a0b69c
Arno Tiemersma <arno.tiemersma@gmail.com>
2016-12-05 22:05:42 +0100

    Solve day 3 part 2

--- 2016/day3/day3.rkt
+++ 2016/day3/day3.rkt
@@ -9,4 +9,23 @@
          (> (+ b c) a)
          (> (+ c a) b))))
 
+(displayln "Part 1:")
 (count identity (map check-triangle input))
+
+(define (check-3-triangles row1 row2 row3)
+  (let* ([tri1 (map first (list row1 row2 row3))]
+         [tri2 (map second (list row1 row2 row3))]
+         [tri3 (map third (list row1 row2 row3))])
+    (count identity (map check-triangle (list tri1 tri2 tri3)))))
+
+(define (check-rotated-triangles input)
+  (define (loop input num-triangles)
+    (if (empty? input)
+        num-triangles
+        (loop (cdddr input) (+ num-triangles (check-3-triangles (first input)
+                                                                (second input)
+                                                                (third input))))))
+    (loop input 0))
+
+(displayln "Part 2:")
+(check-rotated-triangles input)
